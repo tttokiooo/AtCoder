0874fbea20ae87bce8fe800e316b39009703f195
tttokiooo <tt2316tokio@gmail.com>
2019-12-18 23:07:30 +0900

    ABC139

--- ABC139/c.py
+++ ABC139/c.py
@@ -3,7 +3,19 @@ import sys
 def input(): return sys.stdin.readline().rstrip()
 
 def main():
-    pass
+    n = int(input())
+    h = map(int, input().split())
+
+    ans, bef, cnt = 0, 0, 0
+
+    for i in h:
+        if i <= bef:
+            cnt += 1
+        else:
+            ans = max(ans, cnt)
+            cnt = 0
+        bef = i
+    print(max(ans, cnt))
 
 if __name__ == '__main__':
     main()
