e7594848d532d17eabba41b79855114cf997e99b
tttokiooo <tt2316tokio@gmail.com>
2020-01-28 22:00:44 +0900

    timeout時のメッセージ

--- .code_runner/python.sh
+++ .code_runner/python.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
-gtimeout 10 python3 $1 < ${SCRIPT_DIR}/input.txt
+gtimeout 5 python3 $1 < ${SCRIPT_DIR}/input.txt
+STATUS=$?
+if [ "${STATUS}" -eq 124 ];
+then
+    echo "\ntimeout!!"
+fi
