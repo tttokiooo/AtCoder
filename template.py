6d4074e6fc23fef611f821c314b32d028078bba2
tttokiooo <tt2316tokio@gmail.com>
2020-02-01 20:27:55 +0900

    template更新

--- template.py
+++ template.py
@@ -2,7 +2,8 @@ import sys
 def input(): return sys.stdin.readline().rstrip()
 
 def main():
-    map(int, input().split())
+    n = int(input())
+    a, b = map(int, input().split())
 
 if __name__ == '__main__':
     main()
