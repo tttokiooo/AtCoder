a37e3e673b185ad10a4b82263c053f777367ecf9
tttokiooo <tt2316tokio@gmail.com>
2020-04-29 14:43:15 +0900

    python実行shの修正

--- .code_runner/python.sh
+++ .code_runner/python.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
-gtimeout 5 python3 $1 < ${SCRIPT_DIR}/input.txt
+gtimeout 5 python3 $1 < ${SCRIPT_DIR}/../input.txt
 STATUS=$?
-if [ "${STATUS}" -eq 124 ];
-then
+if [ "${STATUS}" -eq 124 ];then
     echo "\ntimeout!!"
 fi
